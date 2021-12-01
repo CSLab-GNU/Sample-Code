9bf10715743567a3531882da543d49d2829b6067
zygn <rikochet@kakao.com>
2021-12-01 20:00:55 +0900

    make waypoint_follow.launch file

---
+++ script/waypoint_follow.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+from numba import njit
+
+import rospy
+from ackermann_msgs.msg import AckermannDriveStamped
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
+
+# TODO: 자율주행 알고리즘 수정
+
+
+"""
+Planner Helpers
+"""
+@njit(fastmath=False, cache=True)
+def nearest_point_on_trajectory(point, trajectory):
+    """
+    Return the nearest point along the given piecewise linear trajectory.
+
+    Same as nearest_point_on_line_segment, but vectorized. This method is quite fast, time constraints should
+    not be an issue so long as trajectories are not insanely long.
+
+        Order of magnitude: trajectory length: 1000 --> 0.0002 second computation (5000fps)
+
+    point: size 2 numpy array
+    trajectory: Nx2 matrix of (x,y) trajectory waypoints
+        - these must be unique. If they are not unique, a divide by 0 error will destroy the world
+    """
+    diffs = trajectory[1:,:] - trajectory[:-1,:]
+    l2s   = diffs[:,0]**2 + diffs[:,1]**2
+    # this is equivalent to the elementwise dot product
+    # dots = np.sum((point - trajectory[:-1,:]) * diffs[:,:], axis=1)
+    dots = np.empty((trajectory.shape[0]-1, ))
+    for i in range(dots.shape[0]):
+        dots[i] = np.dot((point - trajectory[i, :]), diffs[i, :])
+    t = dots / l2s
+    t[t<0.0] = 0.0
+    t[t>1.0] = 1.0
+    # t = np.clip(dots / l2s, 0.0, 1.0)
+    projections = trajectory[:-1,:] + (t*diffs.T).T
+    # dists = np.linalg.norm(point - projections, axis=1)
+    dists = np.empty((projections.shape[0],))
+    for i in range(dists.shape[0]):
+        temp = point - projections[i]
+        dists[i] = np.sqrt(np.sum(temp*temp))
+    min_dist_segment = np.argmin(dists)
+    return projections[min_dist_segment], dists[min_dist_segment], t[min_dist_segment], min_dist_segment
+
+@njit(fastmath=False, cache=True)
+def first_point_on_trajectory_intersecting_circle(point, radius, trajectory, t=0.0, wrap=False):
+    """
+    starts at beginning of trajectory, and find the first point one radius away from the given point along the trajectory.
+
+    Assumes that the first segment passes within a single radius of the point
+
+    http://codereview.stackexchange.com/questions/86421/line-segment-to-circle-collision-algorithm
+    """
+    start_i = int(t)
+    start_t = t % 1.0
+    first_t = None
+    first_i = None
+    first_p = None
+    trajectory = np.ascontiguousarray(trajectory)
+    for i in range(start_i, trajectory.shape[0]-1):
+        start = trajectory[i,:]
+        end = trajectory[i+1,:]+1e-6
+        V = np.ascontiguousarray(end - start)
+
+        a = np.dot(V,V)
+        b = 2.0*np.dot(V, start - point)
+        c = np.dot(start, start) + np.dot(point,point) - 2.0*np.dot(start, point) - radius*radius
+        discriminant = b*b-4*a*c
+
+        if discriminant < 0:
+            continue
+        #   print "NO INTERSECTION"
+        # else:
+        # if discriminant >= 0.0:
+        discriminant = np.sqrt(discriminant)
+        t1 = (-b - discriminant) / (2.0*a)
+        t2 = (-b + discriminant) / (2.0*a)
+        if i == start_i:
+            if t1 >= 0.0 and t1 <= 1.0 and t1 >= start_t:
+                first_t = t1
+                first_i = i
+                first_p = start + t1 * V
+                break
+            if t2 >= 0.0 and t2 <= 1.0 and t2 >= start_t:
+                first_t = t2
+                first_i = i
+                first_p = start + t2 * V
+                break
+        elif t1 >= 0.0 and t1 <= 1.0:
+            first_t = t1
+            first_i = i
+            first_p = start + t1 * V
+            break
+        elif t2 >= 0.0 and t2 <= 1.0:
+            first_t = t2
+            first_i = i
+            first_p = start + t2 * V
+            break
+    # wrap around to the beginning of the trajectory if no intersection is found1
+    if wrap and first_p is None:
+        for i in range(-1, start_i):
+            start = trajectory[i % trajectory.shape[0],:]
+            end = trajectory[(i+1) % trajectory.shape[0],:]+1e-6
+            V = end - start
+
+            a = np.dot(V,V)
+            b = 2.0*np.dot(V, start - point)
+            c = np.dot(start, start) + np.dot(point,point) - 2.0*np.dot(start, point) - radius*radius
+            discriminant = b*b-4*a*c
+
+            if discriminant < 0:
+                continue
+            discriminant = np.sqrt(discriminant)
+            t1 = (-b - discriminant) / (2.0*a)
+            t2 = (-b + discriminant) / (2.0*a)
+            if t1 >= 0.0 and t1 <= 1.0:
+                first_t = t1
+                first_i = i
+                first_p = start + t1 * V
+                break
+            elif t2 >= 0.0 and t2 <= 1.0:
+                first_t = t2
+                first_i = i
+                first_p = start + t2 * V
+                break
+
+    return first_p, first_i, first_t
+
+@njit(fastmath=False, cache=True)
+def get_actuation(pose_theta, lookahead_point, position, lookahead_distance, wheelbase):
+    """
+    Returns actuation
+    """
+    waypoint_y = np.dot(np.array([np.sin(-pose_theta), np.cos(-pose_theta)]), lookahead_point[0:2]-position)
+    speed = lookahead_point[2]
+    if np.abs(waypoint_y) < 1e-6:
+        return speed, 0.
+    radius = 1/(2.0*waypoint_y/lookahead_distance**2)
+    steering_angle = np.arctan(wheelbase/radius)
+    return speed, steering_angle
+
+class PurePursuitPlanner:
+    """
+    Example Planner
+    """
+    def __init__(self, conf, wb):
+        self.wheelbase = wb
+        self.conf = conf
+        self.load_waypoints(conf)
+        self.max_reacquire = 20.
+
+        self.drawn_waypoints = []
+
+    def load_waypoints(self, conf):
+        """
+        loads waypoints
+        """
+        self.waypoints = np.loadtxt(conf['wpt_path'], delimiter=conf['wpt_delim'], skiprows=conf['wpt_rowskip'])
+
+        
+    def _get_current_waypoint(self, waypoints, lookahead_distance, position, theta):
+        """
+        gets the current waypoint to follow
+        """
+        wpts = np.vstack((self.waypoints[:, self.conf['wpt_xind']], self.waypoints[:, self.conf['wpt_yind']])).T
+        nearest_point, nearest_dist, t, i = nearest_point_on_trajectory(position, wpts)
+        if nearest_dist < lookahead_distance:
+            lookahead_point, i2, t2 = first_point_on_trajectory_intersecting_circle(position, lookahead_distance, wpts, i+t, wrap=True)
+            if i2 == None:
+                return None
+            current_waypoint = np.empty((3, ))
+            # x, y
+            current_waypoint[0:2] = wpts[i2, :]
+            # speed
+            current_waypoint[2] = waypoints[i, self.conf['wpt_vind']]
+            return current_waypoint
+        elif nearest_dist < self.max_reacquire:
+            return np.append(wpts[i, :], waypoints[i, self.conf['wpt_vind']])
+        else:
+            return None
+
+    def plan(self, scan_data, odom_data):
+        """
+        gives actuation given observation
+        """
+
+        pose_x = odom_data['pose_x']
+        pose_y = odom_data['pose_y']
+        pose_theta = odom_data['pose_theta']
+        lookahead_distance = odom_data['lookahead_distance']
+        vgain = odom_data['vgain']
+
+        position = np.array([pose_x, pose_y])
+        lookahead_point = self._get_current_waypoint(self.waypoints, lookahead_distance, position, pose_theta)
+
+        if lookahead_point is None:
+            return 2.0, 0.0
+
+        speed, steering_angle = get_actuation(pose_theta, lookahead_point, position, lookahead_distance, self.wheelbase)
+        speed = vgain * speed
+
+        return speed, steering_angle
+
+
+class RosProc:
+    def __init__(self):
+
+        self.robot_scale = rospy.get_param('robot_scale', 0.3302)
+
+        self.pln_params = {
+            'max_speed': rospy.get_param('max_speed', 0.5),
+            'min_speed': rospy.get_param('min_speed', 0.1),
+            'wpt_path': rospy.get_param('wpt_path', 'waypoints.csv'),
+            'wpt_delim': rospy.get_param('wpt_delim', ','),
+            'wpt_rowskip': rospy.get_param('wpt_rowskip', 0),
+            'wpt_xind': rospy.get_param('wpt_xind', 0),
+            'wpt_yind': rospy.get_param('wpt_yind', 1),
+            'wpt_vind': rospy.get_param('wpt_vind', 2),
+            'wpt_thetind': rospy.get_param('wpt_thetind', 3),
+        }
+        self.planner = PurePursuitPlanner(self.pln_params, self.robot_scale)
+        self.pub = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=1)
+        self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback)
+        self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
+
+        self.ackermann = AckermannDriveStamped()
+        self.ackermann.drive.speed = 0.0
+        self.ackermann.drive.steering_angle = 0.0
+
+        self.scan_data = {'ranges': [0] * 1080}
+        self.odom_data = {
+            'pose_x': 0.0,
+            'pose_y': 0.0,
+            'pose_theta': 0.0,
+            'linear_vels_x': 0.0,
+            'lookahead_distance': 0.82461887897713965,
+            'vgain': 0.90338203837889
+        }
+
+    def odom_callback(self, odom_msg):
+        qx = odom_msg.pose.pose.orientation.x
+        qy = odom_msg.pose.pose.orientation.y
+        qz = odom_msg.pose.pose.orientation.z
+        qw = odom_msg.pose.pose.orientation.w
+
+        siny_cosp = 2.0 * (qw * qz + qx * qy)
+        cosy_cosp = 1.0 - 2.0 * (qy * qy + qz * qz)
+
+        current_position_theta = np.arctan2(siny_cosp, cosy_cosp)
+        current_position_x = odom_msg.pose.pose.position.x
+        current_position_y = odom_msg.pose.pose.position.y
+
+        self.odom_data['pose_x'] = current_position_x
+        self.odom_data['pose_y'] = current_position_y
+        self.odom_data['pose_theta'] = current_position_theta
+        self.odom_data['linear_vels_x'] = odom_msg.twist.twist.linear.x
+
+
+    def scan_callback(self, scan_msg):
+        self.scan_data['ranges'] = scan_msg.ranges
+        speed, steering_angle = self.planner.plan(self.scan_data, self.odom_data)
+
+        self.ackermann.drive.speed = speed
+        self.ackermann.drive.steering_angle = steering_angle
+        self.pub.publish(self.ackermann)
+
+
+if __name__ == "__main__":
+    rospy.init_node('fgm_convolution')
+    rate = rospy.Rate(100)
+    app = RosProc()
+    while not rospy.is_shutdown():
+        rate.sleep()
